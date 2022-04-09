c2875c95a02936cb00be3fcdc947d748ec43a940
T1mzhou <zhoutianwenzzza@outlook.com>
2022-04-09 22:57:02 +0800

    add two pointer

---
+++ TwoPointer/415·有效回文串.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    /**
+     * @param s: A string
+     * @return: Whether the string is a valid palindrome
+     */
+    bool isValid(char str)
+    {
+	    return isdigit(str) || isalpha(str);
+    }
+
+    bool isEqual(char firstStr, char secondStr)
+    {
+	    return tolower(firstStr) == tolower(secondStr);
+    }
+
+    bool isPalindrome(string &s) {
+        int left = 0;
+	   int right = s.size() - 1;
+
+	   while ( left < right )
+	   {
+		   while ( left < right && !isValid(s[left]) )
+		   {
+			   left++;
+		   }
+
+		   while ( left < right && !isValid(s[right]) )
+		   {
+			   right--;
+		   }
+
+		   while ( left < right && !isEqual(s[left], s[right]) )
+		   {
+			   return false;
+		   }
+
+		   left++;
+		   right--;
+	   }
+
+	   return true;
+    }
+};
+
